64f471561c76b7332e20e4de2919bf2dd747895c
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-02 16:23:28 -0700

    Completed displaying dummy data

---
+++ src/helpers/list_data.js
@@ -0,0 +1,23 @@
+
+export default [
+    {
+        title: 'Learn React',
+        details: 'Learn reactJS',
+    },
+    {
+        title: 'Build a Server',
+        details: 'Learn how to build a Server for final project'
+    },
+    {
+        title: 'Practice piano',
+        details: 'Learn how to play \'After you\'ve gone\''
+    },
+    {
+        title: 'Exercise',
+        description: 'Row 15k'
+    },
+    {
+        title: 'Exercise',
+        description: 'Pick things up and put them down'
+    }
+];
